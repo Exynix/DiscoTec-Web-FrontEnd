8b143692abe9b5a6ea580259e2ad7e596f6aeafb
Arduousbook <davids_barretom@javeriana.edu.co>
2023-10-11 16:24:45 -0500

    Creation of initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SearchPageComponent } from './search-page/search-page/search-page.component';
+import { SignUpComponent } from './sign-up/sign-up/sign-up.component';
+import { LogInComponent } from './log-in/log-in/log-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    SearchPageComponent,
+    SignUpComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule
