232f70bb073c83a1ada8aa109ec441bd4526cc71
Exynix <davids_barretom@javeriana.edu.co>
2023-10-19 19:04:34 -0500

    Creacion de componentes y avances en landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { UserLoginComponent } from './login/user-login/user-login.component';
+import { HeroSectionComponent } from './landing-page/hero-section/hero-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    UserLoginComponent,
+    HeroSectionComponent
   ],
   imports: [
     BrowserModule
