5afede1f6db627d974581cf429bef07494a6142a
Exynix <davids_barretom@javeriana.edu.co>
2023-10-24 12:42:50 -0500

    Creation of global styles for colors and fonts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { UserLoginComponent } from './login/user-login/user-login.component';
 import { HeroSectionComponent } from './landing-page/hero-section/hero-section.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     UserLoginComponent,
-    HeroSectionComponent
+    HeroSectionComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule
