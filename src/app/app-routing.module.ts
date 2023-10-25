4eb48c4787afe6d97f967b934c55d3d2c0413829
Arduousbook <davids_barretom@javeriana.edu.co>
2023-10-25 18:21:42 -0500

    Change to remove console errors

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {LandingPageComponent} from "./landing-page/landing-page.component";
+import {UserLoginComponent} from "./login/user-login/user-login.component";
+import {SearchPageComponent} from "./search-page/search-page.component";
+
+const routes: Routes = [
+
+  {
+    path: '',
+    component: LandingPageComponent
+  },
+
+  {
+    path: 'login',
+    component: UserLoginComponent
+  },
+
+  {
+    path: "search",
+    component: SearchPageComponent
+  }
+
+]
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,35 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RouterModule, Routes } from '@angular/router'
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { UserLoginComponent } from './login/user-login/user-login.component';
 import { HeroSectionComponent } from './landing-page/hero-section/hero-section.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { TopBarLandingPageComponent } from './top-bar/top-bar-landing-page/top-bar-landing-page.component';
-import { SearchPageComponent } from './search-page/search-page/search-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { UserSignupComponent } from './signup/user-signup/user-signup.component';
 import { AdminSignupComponent } from './signup/admin-signup/admin-signup.component';
 import { AdminLoginComponent } from './login/admin-login/admin-login.component';
+import { AppRoutingModule } from './app-routing.module';
+import {RouterLink} from "@angular/router";
 
-
-
-const routes: Routes = [
-
-  {
-    path: '',
-    component: LandingPageComponent
-  },
-
-  {
-    path: '/login',
-    component: UserLoginComponent
-  }
-
-
-]
 
 @NgModule({
   declarations: [
@@ -49,7 +34,9 @@ const routes: Routes = [
     AdminLoginComponent
   ],
   imports: [
-    BrowserModule, RouterModule.forRoot(routes)
+    BrowserModule,
+    AppRoutingModule,
+    RouterLink
   ],
   providers: [],
   bootstrap: [AppComponent]
