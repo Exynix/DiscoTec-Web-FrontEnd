cc664f8c2bdbb3d69cfd4e914fa921d82fc1c1e4
Exynix <davids_barretom@javeriana.edu.co>
2023-10-27 08:56:44 -0500

    Advances on login page implementation

--- src/app/login/user-login/user-login.component.ts
+++ src/app/login/user-login/user-login.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.css']
+  styleUrls: ['./user-login.component.css', '../login.component.css']
 })
 export class UserLoginComponent {
 
