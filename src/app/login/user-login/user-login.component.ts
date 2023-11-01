a48f4f08c4c4617c16cc37747c2bdaac99447175
Arduousbook <davids_barretom@javeriana.edu.co>
2023-11-01 01:20:00 -0500

    Addition of local storage methods for user

--- src/app/login/user-login/user-login.component.ts
+++ src/app/login/user-login/user-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-user-login',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-login.component.css', '../login.component.css']
 })
 export class UserLoginComponent {
+
+  emailField = new FormControl ('',
+    [
+      Validators.required,
+      Validators.email
+    ])
+
+  passwordField = new FormControl ('',
+    [
+      Validators.required
+    ])
+
+  constructor() {
+  }
+
+
 
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Owner} from "../model/owner.model";
+import {Client} from "../model/client.model";
 
 @Injectable({
   providedIn: 'root'
@@ -18,4 +19,27 @@ export class LoginService {
 
 
    */
+
+  saveClientInLocalStorage(client: Client){
+
+      // 1. Parse user to string
+      // 2. Save it in local storage in a key-value pair.
+    const clientString = JSON.stringify(client)
+    localStorage.setItem("currentClient", clientString)
+
+  }
+
+  getClientFromLocalStorage(): Client | null {
+    // 1. Get the user string from local storage
+    // 2. Parse it to its data structure
+    const savedClientString  = localStorage.getItem("currentClient")
+
+    if (savedClientString != null){
+      const userInfo = JSON.parse(savedClientString)
+      return userInfo
+    }
+
+    console.log("Failed to get client data because its null.")
+    return null
+  }
 }
